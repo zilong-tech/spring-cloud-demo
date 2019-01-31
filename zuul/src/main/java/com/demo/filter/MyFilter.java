e6c34244aa87d3c9f30c11594cffc084f8046a6f
zouyongsheng <zys@zhenanxin.com>
2019-01-31 10:40:42 +0800

    code commit

---
+++ zuul/src/main/java/com/demo/filter/MyFilter.java
@@ -0,0 +1,61 @@
+package com.demo.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Description:
+ * <p>
+ * Create on 2019/01/31
+ *
+ * @author zouyongsheng
+ */
+@Component
+@Slf4j
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        //{} 占位符
+        log.info("\n请求信息：{}\t{}",request.getMethod(),request.getRequestURL());
+        String token = request.getParameter("token");
+        if(StringUtils.isEmpty(token)){
+            log.error("用户未登录");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            try {
+                HttpServletResponse response = context.getResponse();
+               // response.setCharacterEncoding("UTF-8");
+                response.getWriter().write("用户未登录");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
